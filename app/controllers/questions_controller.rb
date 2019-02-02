447543606552308049d6d202006b97e6d9fd4df0
Sarah Morrissey <s.morrissey@gatech.edu>
2019-02-01 21:48:40 -0500

    questions form keeps questions on failure

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,24 +1,32 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @book = @question.book
   end
 
-  def new; end
+  def new
+    @book = Book.new
+  end
 
   def create
-    book = Book.find(book_id_param)
-    cookies[:book_id] = book.id
-    return unless book.update(questions_params)
-
-    flash[:success] = 'Question(s) saved'
+    id = params[:book][:book_id]
+    book = Book.find(id)
+    cookies[:book_id] = id
+    if book.update(questions_params)
+      flash[:success] = 'Question(s) saved'
+      @book = Book.new
+    else
+      flash[:danger] = book.errors.full_messages.join ', '
+      @book = Book.new questions_params
+    end
     render :new
   end
 
   def index; end
 
   def practice
-
     data = practice_data
     books = data[:books]
     books.delete('')
@@ -32,7 +40,7 @@ class QuestionsController < ApplicationController
 
   def next_question
     if session[:practice_books].nil? || session[:practice_books].empty?
-      flash[:danger] = 'No questions found.'
+      flash[:danger] = 'No questions founAttendance.where(:user_id => @user.id)d.'
       return redirect_to '/questions/practice'
     end
 
@@ -86,10 +94,6 @@ class QuestionsController < ApplicationController
   end
 
   private
-
-  def book_id_param
-    params.require(:book).permit(:book_id)[:book_id]
-  end
 
   def questions_params
     params.require(:book).permit(questions_attributes: %i[user_id question answer page])
