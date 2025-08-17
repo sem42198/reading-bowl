cdb97da403eae2dfbbe03a2d8d50854cbe62a4ea
Sarah Morrissey <s.morrissey@gatech.edu>
2025-08-17 10:49:08 -0400

    get prod to run

--- app/helpers/concerns/welcome_helper.rb
+++
@@ -1,2 +0,0 @@
-module Concerns::WelcomeHelper
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2018_12_14_013038) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_17_143516) do
   create_table "answer_events", force: :cascade do |t|
     t.integer "user_id"
     t.integer "question_id"
